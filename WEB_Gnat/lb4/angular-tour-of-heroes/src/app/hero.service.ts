ee32dc5147d1a83f4143741f776d013e3d7bc7e7
0xKolyanus <mukola.1zt.@gmail.com>
2021-11-11 12:34:42 +0200

    start do 9 lb WEB

--- WEB_Gnat/lb4/angular-tour-of-heroes/src/app/hero.service.ts
+++ WEB_Gnat/lb4/angular-tour-of-heroes/src/app/hero.service.ts
@@ -3,17 +3,25 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes'; 
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService HELLO');
     return of(HEROES);
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 
   getHero(id: number): Observable<Hero> {
