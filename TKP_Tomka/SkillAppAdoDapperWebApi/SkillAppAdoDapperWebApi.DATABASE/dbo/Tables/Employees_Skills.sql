e3e4be88fca73883807f9cea31b716f9f7a58516
0xKolyanus <mukola.1zt.@gmail.com>
2021-11-11 11:47:01 +0200

    del TKP

--- TKP_Tomka/SkillAppAdoDapperWebApi/SkillAppAdoDapperWebApi.DATABASE/dbo/Tables/Employees_Skills.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Employees_Skills] (
-    [Id]          INT NOT NULL,
-    [Employee_Id] INT NOT NULL,
-    [Skill_Id]    INT    NOT NULL,
-    [Score_Id]    INT    NOT NULL,
-    CONSTRAINT [PK_Employees_Skills] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
